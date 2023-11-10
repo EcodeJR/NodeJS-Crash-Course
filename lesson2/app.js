1e181ea46338106ad30e20405a632974ed91f37e
EcodeJR <emmanueldcode@gmail.com>
2023-11-10 16:31:41 +0100

    New lesson

--- lesson2/app.js
+++ lesson2/app.js
@@ -3,24 +3,36 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //Listen for requset
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    const blogs = [
+        {title: "Blog 1", snippet:"This is the first blog"},
+        {title: "Blog 2", snippet:"This is the second blog"},
+        {title: "Blog 3", snippet:"This is the third blog"}
+    ];
     // res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home', blogs });
 });
 app.get('/about', (req, res) => {
     // res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' })
 });
 
-//redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// //redirect
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+})
 
 //404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
