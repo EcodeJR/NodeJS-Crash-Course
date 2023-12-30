18a3cd23ad1eb1cd2ef69755e7e822ced6facce5
EcodeJR <emmanueldcode@gmail.com>
2023-12-30 12:41:48 +0100

    project completed.

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
-const Blog = require('./modules/blog');
-const { render } = require('ejs');
+const blogRoutes = require('./routes/blogroutes');
+//const { render } = require('ejs');
 //const { result } = require('lodash');
 
 
@@ -99,55 +98,8 @@ app.get('/about', (req, res) => {
 //     res.redirect('/about');
 // });
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
-
-//posting a blog to the DB.
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' });
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            res.json({ redirect: '/blogs' })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-})
+//moved to blog routes
+app.use('/blogs', blogRoutes);
 
 //404 page
 app.use((req, res) => {
